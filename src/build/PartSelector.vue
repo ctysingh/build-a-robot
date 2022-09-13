7551d3ea188c431c882cbfe0d08d50fb158cccd7
ctysingh <chaitanyasingh@gmail.com>
2022-09-13 08:41:53 -0400

    handling event from child component

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -44,12 +44,14 @@ export default {
         this.selectedPartIndex,
         this.parts.length,
       );
+      this.$emit('partSelected', this.selectedPart);
     },
     selectPreviousPart() {
       this.selectedPartIndex = getPreviousValidIndex(
         this.selectedPartIndex,
         this.parts.length,
       );
+      this.$emit('partSelected', this.selectedPart);
     },
 
   },
